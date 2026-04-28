f3a2db20db7ff5120e87bcade623359c04f6c7ca
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2026-04-28 22:12:17 +0530

    Two Sum Using Functional Way
    Two Sum Using Functional Way

--- MyMain.java
+++ MyMain.java
@@ -1,6 +1,13 @@
 public class MyMain {
     public static void main(String[] args) {
         System.out.println("DSA Using Latest Java Features");
+        System.out.println("=".repeat(60) + "\n");
+        
         MyGetCharsDemo.executeDemo();
+        
+        System.out.println("\n" + "=".repeat(60) + "\n");
+        
+        TwoSumDemo.executeDemo();
+        TwoSumDemo.demonstrateFunctionalApproach();
     }
 }
